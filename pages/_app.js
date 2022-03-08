8c286cb1bb4f42d13bd8c80a844ec3499362c354
Amit panchal <04amitpanchal>
2022-03-08 17:51:04 +0530

    Converting lending pages into next js.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import "../assets/css/style.css";
+import "../assets/css/animate.css";
+import "../assets/css/bootstrap.min.css";
+import "../assets/css/default.css";
+import "../assets/css/font-awesome.min.css";
+import "../assets/css/lineIcons.css";
+import "../assets/css/magnific-popup.css";
+import "../assets/css/slick.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
